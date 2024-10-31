fbccf327f149522cd0597f1fed739ad0c68e3345
jameskimani001 <jameskim092t@gmail.com>
2024-10-31 11:21:36 +0300

    components basics completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,18 @@
-import React from "react";
-
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-{/* write an <About> component here */}
+import React from 'react';
+import './App.css';  // Import the CSS file
+import Navbar from './Navbar';
+import Home from './Home';
+import About from './About';
 
 
-function App() {
+const App = () => {
   return (
     <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Navbar />
+      <Home />
+      <About />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h6 className='nav'>i'm a link</h6>
+
+    </nav>
+  );
+};
+
+export default Navbar;
